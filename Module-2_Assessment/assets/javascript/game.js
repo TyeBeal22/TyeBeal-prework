c466acfdcd0b7cda0e92a4a663496221dfb069b1
Darth Tye <53497335+TyeBeal22@users.noreply.github.com>
2020-04-16 08:03:48 -0400

    Add files via upload

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,223 @@
+
+
+
+
+wordGame = function() {
+
+
+  var wins;
+  var categories;         // theme
+  var chosenCategory;
+  var word ;
+  var guess ;
+  var geusses = [ ];
+  var lives ;
+  var counter ;
+  var space;
+
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+
+  var showLives = document.getElementById("mylives");
+  var showCatagory = document.getElementById("scatagory");
+
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+
+
+  // choose themes
+  var chooseT = function () {
+    if (chosenCategory === categories[0]) {
+      catagoryName.innerHTML = "Best sports athletes";
+    } else if (chosenCategory === categories[1]) {
+      catagoryName.innerHTML = "Best TV shows";
+    } else if (chosenCategory === categories[2]) {
+      catagoryName.innerHTML = "Favorite Foods";
+    }
+  }
+
+  // Create geusses ul
+   result = function() {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+
+  // guesses
+   comments = function() {
+    showLives.innerHTML = "You have " + lives + " guesses";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        wins++;
+        showLives.innerHTML = "You win";
+      }
+    }
+  }
+
+
+  var animate = function () {
+    var drawMe = lives ;
+    drawArray[drawMe]();
+  }
+
+
+   // Hangman
+  canvas =  function(){
+
+    myStickman = document.getElementById("stickman");
+    context = myStickman.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = "#fff";
+    context.lineWidth = 2;
+  };
+
+    head = function(){
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI*2, true);
+      context.stroke();
+    }
+
+  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke();
+}
+
+   frame1 = function() {
+     draw (0, 150, 150, 150);
+   };
+
+   frame2 = function() {
+     draw (10, 0, 10, 600);
+   };
+
+   frame3 = function() {
+     draw (0, 5, 70, 5);
+   };
+
+   frame4 = function() {
+     draw (60, 5, 60, 15);
+   };
+
+   torso = function() {
+     draw (60, 36, 60, 70);
+   };
+
+   rightArm = function() {
+     draw (60, 46, 100, 50);
+   };
+
+   leftArm = function() {
+     draw (60, 46, 20, 50);
+   };
+
+   rightLeg = function() {
+     draw (60, 70, 100, 100);
+   };
+
+   leftLeg = function() {
+     draw (60, 70, 20, 100);
+   };
+
+  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1];
+
+
+  // OnClick Function
+   check = function () {
+    list.onclick = function () {
+      var geuss = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        }
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+      } else {
+        comments();
+      }
+    }
+  }
+
+
+  // Play
+  play = function () {
+    categories = [
+        ["tom brady", "michael jordan", "lebron james", "tiger woods", "mike trout","christiano renaldo"],
+        ["game of thrones", "ozark", "blue bloods", "finding-nemo", "jaws"],
+        ["shrimp", "chicken", "pizza", "steak", "bacon"]
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    chooseT();
+    canvas();
+  }
+
+  play();
+
+
+   // Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
+}
+
+wordGame();
